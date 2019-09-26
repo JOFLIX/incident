714594948e1c2ed9b855ff92cc9c03db770e47b0
JOFLIX <joflixooko@outlook.com>
2019-09-26 16:43:46 +0300

    defined thje routings

--- incident/urls.py
+++ incident/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    path('tatu/',include('tatuadmin.urls')),
+    path('',include('tatuadmin.urls')),
 
 ]
 
